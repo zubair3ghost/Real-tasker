ccce024a2f034abb6a1aaf9b59b529082c0fc3b3
zubair <zubairahmad29278@gmail.com>
2024-03-18 20:12:44 +0500

    change is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthenticationModule } from './authentication/authentication.module';
-import { HeaderComponent } from './header/header.component';
+ 
 import { LoginComponent } from './authentication/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+ 
   ],
   imports: [
     BrowserModule,
